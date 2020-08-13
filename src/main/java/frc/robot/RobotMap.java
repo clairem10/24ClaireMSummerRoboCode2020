ea45b0438787897fb3179359f475d5682e821384
Claire Miao <claire.q.miao@gmail.com>
2020-08-13 13:53:32 -0700

    Create RobotMap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -1,9 +1,13 @@
 package frc.robot;
 public class RobotMap
 {
-    public static final int leftMaster = 0;
-    public static final int leftFollower = 0;
-    public static final int rightMaster = 0;
-    public static final int rightFollower = 0;
-
+    public static final int LM = 0;
+    public static final int LF = 0;
+    public static final int RM = 0;
+    public static final int RF = 0;
+    public static final int LOOP_INDEX=0;
+    public static final int EXTENDER_FORWARD=0;
+    public static final int EXTENDER_REVERSE=0;
+    public static final int FLOWER_FORWARD=0;
+    public static final int FLOWER_REVERSE=0;
 }
