0e48c7e21daa3656e62aebc49f5db186931edba6
Claire Miao <claire.q.miao@gmail.com>
2020-08-14 10:16:39 -0700

    Added Intake ports

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -10,7 +10,7 @@ public class RobotMap
     public static final int EXTENDER_REVERSE=0;
     public static final int FLOWER_FORWARD=0;
     public static final int FLOWER_REVERSE=0;
-    public static final int IM = 0;
-    public static final int INTAKE_FORWARD = 0;
+    public static final int IM = 10;
+    public static final int INTAKE_FORWARD = 7;
     public static final int INTAKE_REVERSE = 0;
 }
