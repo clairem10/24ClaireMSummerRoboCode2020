39585410ee536fd0e7876a4f2cf77d3190c04514
Claire Miao <claire.q.miao@gmail.com>
2020-08-14 12:13:23 -0700

    Bind ToggleIntakeSolenoid to button

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -3,7 +3,7 @@ package frc.robot;
 import harkerrobolib.wrappers.HSGamepad;
 import harkerrobolib.wrappers.XboxGamepad;
 import frc.robot.commands.SpinIntake;
-
+import frc.robot.commands.ToggleIntakeSolenoid;
 
 public class OI
 {
@@ -16,6 +16,7 @@ public class OI
         driverGamepad=new XboxGamepad(0);
         operatorGamepad=new XboxGamepad(1);
         driverGamepad.getButtonB().whilePressed(new SpinIntake(0.5));
+        driverGamepad.getButtonA().toggleWhenPressed(new ToggleIntakeSolenoid());
     }
 
     public static OI getInstance()
