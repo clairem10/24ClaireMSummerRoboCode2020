69efeebbb44c9988ba6588de49094c02543f3556
Claire Miao <claire.q.miao@gmail.com>
2020-08-14 09:41:11 -0700

    Created Intake subsystem

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,49 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import harkerrobolib.wrappers.HSTalon;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import frc.robot.RobotMap;
+
+public class Intake extends SubsystemBase
+{
+    private HSTalon intakeMotor;
+    private DoubleSolenoid intakeSolenoid;
+    private static Intake it;
+
+    private Intake ()
+    {
+        intakeMotor = new HSTalon(RobotMap.IM);
+        intakeSolenoid = new DoubleSolenoid(RobotMap.INTAKE_FORWARD, RobotMap.INTAKE_REVERSE);
+    }
+
+    public static Intake getInstance()
+    {
+        if(it==null) {
+            it = new Intake();
+        } 
+        return it;
+    }
+
+    public HSTalon getIntakeMotor()
+    {
+        return intakeMotor;
+    }
+
+    public DoubleSolenoid getIntakeSolenoid()
+    {
+        return intakeSolenoid;
+    }
+
+    public DoubleSolenoid.Value invertValue (DoubleSolenoid.Value value)
+    {
+        if (value == DoubleSolenoid.Value.kForward) 
+        {
+            return DoubleSolenoid.Value.kReverse;
+        }
+        else
+        {
+            return DoubleSolenoid.Value.kForward;
+        }
+    }
+}
